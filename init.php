98defa5802cd32392cfc91ee281691664962c2de
Sergey Sadovikow <nexxerr@gmail.com>
2022-04-19 10:16:47 +0300

    Update init.php

--- init.php
+++ init.php
@@ -1,3 +1,7 @@
 if (file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/CMain.php")) {
     require_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/CMain.php");
 }
+
+if (file_exists($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/constants.php")) {
+    require_once($_SERVER["DOCUMENT_ROOT"]."/local/php_interface/include/constants.php");
+}
