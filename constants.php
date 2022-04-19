d42a14e851d2c05182e30d4dbdc318da05ccc64d
Sergey Sadovikow <nexxerr@gmail.com>
2022-04-19 10:16:30 +0300

    Create constants.php

---
+++ constants.php
@@ -0,0 +1,5 @@
+<?
+define("IBLOCK_ID_CATALOG", getIdIBFromXML_ID('catalog')); //ID инфоблока каталога
+define("IBLOCK_TYPE_CATALOG", "1c_catalog");
+define("IBLOCK_ID_SKU_CATALOG", getIdIBFromXML_ID('variants'));
+?>
