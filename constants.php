4b1690f574e15e7280ef2bfb06f24dd55e3b30e6
Sergey Sadovikow <nexxerr@gmail.com>
2022-04-21 11:56:29 +0300

    highloadblock add

--- constants.php
+++ constants.php
@@ -1,5 +1,4 @@
 <?
 define("IBLOCK_ID_CATALOG", getIdIBFromXML_ID('catalog')); //ID инфоблока каталога
-define("IBLOCK_TYPE_CATALOG", "1c_catalog");
-define("IBLOCK_ID_SKU_CATALOG", getIdIBFromXML_ID('variants'));
+define('HIGHLOAD_BLOCK_COLORS', getIdHighLoadBlockFromName('higloadblock_code')); // ID highload infoblock
 ?>
